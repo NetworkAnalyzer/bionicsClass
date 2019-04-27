cc2d8deed9f951ff05aef6d2d83ff13a854abd2a
Daiki <nojiri1098@gmail.com>
2019-04-27 19:04:15 +0900

    WIP: Add root2.py

--- 01_gradient_descent/root2.py
+++ 01_gradient_descent/root2.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
+END_CONDITION = 0.00001
 
 def function(x):
-    return 2 - x**2
+    return x**2 - 2
 
 def function_prime(x):
-    return -2 * x
+    return 2 * x
 
 def should_exit(e):
-    return e < 0.01
+    return 
 
 def main(x_init, learning_rate):
     
     x = x_init
     x_history = [x]
 
-    while(1):
+    for _ in range(0, 100):
         x_new = x - learning_rate * function_prime(x)
-        x_history.append = x_new
+        print(x_new)
 
-        if should_exit(abs(x - x_new)):
-            exit()
+        error = abs(x - x_new)
+        print('error:', error)
+        if error < END_CONDITION:
+            break
 
-def if __name__ == "__main__":
-    main(0.1, 0.1)
+        x = x_new
+        x_history.append(x_new)
+    
+    print(x_history)
+
+if __name__ == "__main__":
+    main(1, 0.01)
