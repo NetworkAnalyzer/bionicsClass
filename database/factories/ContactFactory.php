a371cf120789230115ca7a1bc44665c8fcf8eb54
Daiki Nojiri <nojiri1098@gmail.com>
2020-02-13 13:04:26 +0900

    Install populated-factory (#7)
    * Install populated-factory
    
    * Generate ModelFactory using populated-factory command
    
    php artisan make:populated-factory User --force
    php artisan make:populated-factory Admin
    php artisan make:populated-factory Contact

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -4,13 +4,11 @@ use Faker\Generator as Faker;
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-$factory->define(\App\User::class, function (Faker $faker) {
+$factory->define(\App\Contact::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => $faker->dateTime,
-        'password' => '$2y$10$uTDnsRa0h7wLppc8/vB9C.YqsrAZwhjCgLWjcmpbndTmyo1k5tbRC',
-        'remember_token' => $faker->sha1,
+        'message' => $faker->text(16383),
         'created_at' => $faker->dateTime,
         'updated_at' => $faker->dateTime,
     ];
