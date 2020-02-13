0ec0d35ea27b139366ed675f3cfdfbf9897b955f
Daiki <nojiri1098@gmail.com>
2020-02-13 13:00:36 +0900

    Generate ModelFactory using populated-factory command
    add       新しい機能を追加した
    remove    不要な機能を削除した
    modify    お客さんの注文で仕様変更した
    update    機能を修正した (仕様変更ではない)
    fix       バグ修正
    clean     コードを綺麗にした (余計なソースを消す，インデントを整える)
    refactor  リファクタリング
    comment   コメントを追加した，編集した
    change    ファイル名や変数名を変更した
    wip       中途半端だけどコミットした (とりま見せたい，急な引き継ぎ)

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-$factory->define(\App\User::class, function (Faker $faker) {
+$factory->define(\App\Admin::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
